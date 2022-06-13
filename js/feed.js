2e48c2e9cc54e896f3a8206b5cb8bf4d0d215af4
fraol0912 <fraol0912@gmail.com>
2022-06-13 03:31:43 +0300

    more interactivity

--- js/feed.js
+++ js/feed.js
@@ -3,9 +3,44 @@ import setupHeaderMenus from "./utils/headerMenus.js";
 import setupPinGrid from "./utils/pinGrid.js";
 import setupPinMenus from "./utils/pinMenus.js";
 
+const tags = [
+  "bathroom",
+  "bottle",
+  "deck",
+  "fern",
+  "scandinavian",
+  "astronaut",
+  "flowers",
+  "green",
+  "mirrors",
+  "desert",
+  "design",
+  "nature",
+  "water",
+  "waterfall",
+  "dancing",
+  "bed",
+  "laptop",
+  "computer",
+  "sunlight",
+  "sun",
+  "sunset",
+  "sunrise",
+];
+
 window.addEventListener("load", () => {
   setupSearchBar();
   setupHeaderMenus();
   setupPinGrid();
   setupPinMenus();
+
+  const chips = document.querySelector("#chips");
+
+  const chipButtons = tags.map((tag) => {
+    const button = document.createElement("button");
+    button.innerText = tag;
+    return button;
+  });
+
+  chips.append(...chipButtons);
 });
